517fc49bda67727f575d2c6c0e66fcabc4bb3d56
MariaBelenGuaranda <mguarand@espol.edu.ec>
2018-04-25 10:44:24 -0500

    hobo and datalogger parsers

--- csv/csv_parsers_module.py
+++ csv/csv_parsers_module.py
@@ -1,36 +1,71 @@
 import configparser
 from datetime import datetime
+from dateutil.parser import *
 
-def parseCSV(file, sensor_type):
-	"""Parse a csv from any meteorological station, and save measures in the database"""
+def parseHOBO(file):
+	"""Parse HOBO file"""
+	STATION="HOBO"
 	config = configparser.ConfigParser()
-	config.read("meteo_stations.ini")
-	if sensor_type not in config:
-		print ("Meteorological station not configured. Exit...")
+	config.read("meteo_stations.cfg")
+	if "HOBO" not in config.sections():
+		print "Meteorological station not configured. Exit..."
 		sys.exit()
 	file = open(file,"r")
-	headers= int (config[sensor_type][headers])
+	headers= int (config.get(STATION,"headers"))
 	#read verbose headers
 	for i in range(headers):
 		file.readline()
 	#start reading the file
 	for line in file:
+		print line
+		measures = line.strip().split("\t")
+		#Parse timestamp as it can be a datetime or date and time independent fields
+		datetime_format = str(config.get(STATION,"datetime_format"))
+		date_pos = int(config.get(STATION,"date_pos"))-1
+		datetime = measures[date_pos]
+		ts = parseDatetime(datetime)
+		fields = config.get(STATION,"fields")
+		fields = fields.strip().split(",")
+		print measures 
+		print "\n"
+
+def parseCF200(file):
+	"""Parse Datalogger CF200 file"""
+	STATION = "CF200"
+	config = configparser.ConfigParser()
+	config.read("meteo_stations.cfg")
+	if "CF200" not in config.sections():
+		print "Meteorological station not configured. Exit..."
+		sys.exit()
+	file = open(file,"r")
+	headers= int (config.get(STATION,"headers"))
+	#read verbose headers
+	for i in range(headers):
+		file.readline()
+	#start reading the file
+	for line in file:
+		print line
 		measures = line.strip().split(",")
 		#Parse timestamp as it can be a datetime or date and time independent fields
-		datetime_format = config[sensor_type]["datetime_format"]
-		date_pos = config[sensor_type]["date_pos"]
+		date_pos = int(config.get(STATION,"date_pos"))-1
+		time_pos = date_pos + 1
+		date = measures[date_pos]
+		time = measures[time_pos]
+		ts = parseDatetime(date,time)
+		fields = config.get(STATION,"fields")
+		fields = fields.strip().split(",")
+		print measures 
+		print "\n"
 
-		if(config[sensor_type]["time_pos"]=="None"):
-			ts = datetime.strptime(measures[date_pos], datetime_format)
+def parseDatetime(date, time = None):
+	if time != None:
+		date = date.strip()
+		time = time.strip()
+		ts = parse(date+" "+time)
+	else:
+		ts = parse(date)
+	return ts
 
-		else:
-			time_format = config[sensor_type]["time_format"]
-			datetime_format = datetime_format+" "+time_format
-			time_pos = config[sensor_type]["time_pos"]
-
-			date = measures[date_pos]
-			time = measures[time_pos]
-			ts = datetime.strptime(date+time, datetime_format)
 
 
 
